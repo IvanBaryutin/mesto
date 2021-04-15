09abbe7109c2b1adfda3425145c656b8b703d9db
Ivan Baryutin <vanessru@yandex.ru>
2021-04-15 21:25:58 +0300

    feature: Добавить функционал для form submit

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
+
+
 document.addEventListener('DOMContentLoaded', function(){ // Аналог $(document).ready(function(){
 
-  let page = document.querySelector('.page');
+  //let page = document.querySelector('.page');
 
-  let ProfileEditLink = page.querySelector('.profile__edit-link');
+  let ProfileEditLink = document.querySelector('.profile__edit-link');
   ProfileEditLink.addEventListener('click', openPopup);
   let closePopupLink = document.querySelector('.popup__close');
   closePopupLink.addEventListener('click', closePopup);
+
+  let profileName = document.querySelector('.profile__name');
+  let profileJob = document.querySelector('.profile__subtitle');
+
+
+  let popup = document.querySelector('.popup');
+  let FormNameInput = popup.querySelector('input[name="name"]');
+  let FormJobInput = popup.querySelector('input[name="job"]');
+  let form = popup.querySelector('.form');
+
+
+
+  form.addEventListener('submit', formSubmitHandler);
+
+
+  function openPopup() {
+    FormNameInput.value = profileName.innerText;
+    FormJobInput.value = profileJob.innerText;
+    popup.classList.add('popup_opened');
+  }
+
+  function closePopup() {
+    popup.classList.remove('popup_opened');
+  }
+
+  function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+    profileName.textContent = FormNameInput.value;
+    profileJob.textContent = FormJobInput.value;
+    closePopup();
+  }
+
 });
 
 
-function openPopup() {
-  let profileName = document.querySelector('.profile__name').innerText;
-  let profileJob = document.querySelector('.profile__subtitle').innerText;
 
-  popup = document.querySelector('.popup');
 
-  let FormNameInput = popup.querySelector('input[name="name"]');
-  let FormJobInput = popup.querySelector('input[name="job"]');
 
-  FormNameInput.value = profileName;
-  FormJobInput.value = profileJob;
-  popup.classList.add('popup_opened');
-}
 
-function closePopup() {
-  console.log('test');
-  popup.classList.remove('popup_opened');
-}
+
