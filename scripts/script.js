aee9edcd5d0ba9c5f214974159ac1c6fcae11f9b
Ivan Baryutin <vanessru@yandex.ru>
2021-04-27 22:46:23 +0300

    feature: Добавить функцию добавления карточек в elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,37 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const initialCards2 = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  }
+];
+
   let profileEditLink = document.querySelector('.profile__edit-button');
   let closePopupLink = document.querySelector('.popup__close');
 
@@ -30,6 +64,52 @@
     closePopup();
   }
 
+  /*
+  function addElementsCards() {
+    const elementTemplate = document.querySelector('#element').content;
+    const elementsContainer = document.querySelector('.elements');
+    //console.log(elementTemplate);
+
+    //клонируем
+    const element = elementTemplate.querySelector('.element').cloneNode(true);
+    const element1 = elementTemplate.querySelector('.element').cloneNode(true);
+
+    // наполняем содержимым
+    element.querySelector('.element__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
+    element.querySelector('.element__title').textContent = 'Архыз';
+
+    // наполняем содержимым
+    element1.querySelector('.element__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
+    element1.querySelector('.element__title').textContent = 'Архыз';
+
+    // отображаем на странице
+    elementsContainer.append(element, element1);
+
+  }
+  */
+
+  function addElementsCards(userElementsData) {
+    const elementTemplate = document.querySelector('#element').content;
+    const elementsContainer = document.querySelector('.elements');
+
+    // создадим из массива дел массив элементов
+    const cardsElements = userElementsData.map(el => {
+
+      //клонируем
+      const listItem = elementTemplate.querySelector('.element').cloneNode(true);
+
+      // наполняем содержимым
+      listItem.querySelector('.element__title').textContent = el['name'];
+      listItem.querySelector('.element__image').src = el['link'];
+      return listItem;
+    });
+    // добавим элементы в DOM, «разложив» массив
+    elementsContainer.append(...cardsElements);
+
+  }
+
   form.addEventListener('submit', formSubmitHandler);
   closePopupLink.addEventListener('click', closePopup);
   profileEditLink.addEventListener('click', openPopup);
+  addElementsCards(initialCards);
+  addElementsCards(initialCards2);
