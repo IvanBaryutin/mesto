8fbff3433224da568406a980a5d4299c8c946580
Ivan Baryutin <vaness97@gmail.com>
2021-04-29 12:59:48 +0300

    fix: проверить проект по чеклисту

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//Массив данных для карточек elements
 const initialCards = [
   {
     name: 'Архыз',
@@ -25,164 +26,143 @@ const initialCards = [
   }
 ];
 
-const initialCards2 = [
+//Тестовый массив для добавления
+const initialCardsTest = [
   {
     name: 'Городок',
     link: 'https://images.unsplash.com/photo-1619550148107-ec8561f7cbac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80'
   }
 ];
 
-  let profileEditLink = document.querySelector('.profile__edit-button');
-  let addButton = document.querySelector('.profile__addbutton');
+//Кнопки
+const profileEditLink = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__addbutton');
+const closePopupLinkProfile = document.querySelector('.popup__close_content_profile');
+const closePopupLinkArticle = document.querySelector('.popup__close_content_article');
+const closePopupLinkImage = document.querySelector('.popup__close_content_image');
 
 
-  let closePopupLinkProfile = document.querySelector('.popup__close_content_profile');
-  let closePopupLinkArticle = document.querySelector('.popup__close_content_article');
-  let closePopupLinkImage = document.querySelector('.popup__close_content_image');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__subtitle');
 
-  let profileName = document.querySelector('.profile__name');
-  let profileJob = document.querySelector('.profile__subtitle');
+//Модальные окна
+const popupEditProfile = document.querySelector('.popup_content_profile');
+const popupAddArticle = document.querySelector('.popup_content_article');
+const popupImage = document.querySelector('.popup_content_image');
 
-  let popupEditProfile = document.querySelector('.popup_content_profile');
-  let popupAddArticle = document.querySelector('.popup_content_article');
-  let popupImage = document.querySelector('.popup_content_image');
+//Поля ввода форм
+const formNameInput = popupEditProfile.querySelector('.form__text-input_name_name');
+const formJobInput = popupEditProfile.querySelector('.form__text-input_name_job');
+const formTitleInput = popupAddArticle.querySelector('.form__text-input_name_title');
+const formLinkInput = popupAddArticle.querySelector('.form__text-input_name_link');
 
-  let formNameInput = popupEditProfile.querySelector('.form__text-input_name_name');
-  let formJobInput = popupEditProfile.querySelector('.form__text-input_name_job');
+//Формы
+const formEditProfile = popupEditProfile.querySelector('.form');
+const formAddArticle = popupAddArticle.querySelector('.form');
 
-  let formTitleInput = popupAddArticle.querySelector('.form__text-input_name_title');
-  let formLinkInput = popupAddArticle.querySelector('.form__text-input_name_link');
+function openPopupEditProfile() {
+  formNameInput.value = profileName.textContent;
+  formJobInput.value = profileJob.textContent;
+  popupEditProfile.classList.add('popup_opened');
+}
 
-  let formEditProfile = popupEditProfile.querySelector('.form');
-  let formAddArticle = popupAddArticle.querySelector('.form');
+function openPopupAddArticle() {
+  popupAddArticle.classList.add('popup_opened');
+}
 
 
-  function openPopupEditProfile() {
-    formNameInput.value = profileName.textContent;
-    formJobInput.value = profileJob.textContent;
-    popupEditProfile.classList.add('popup_opened');
+function closePopupEditProfile() {
+  popupEditProfile.classList.remove('popup_opened');
+}
+
+function closePopupAddArticle() {
+  popupAddArticle.classList.remove('popup_opened');
+}
+
+function closePopupImage() {
+  popupImage.classList.remove('popup_opened');
+}
+
+function editProfileSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = formNameInput.value;
+  profileJob.textContent = formJobInput.value;
+  closePopupEditProfile();
+}
+
+function addArticleSubmitHandler (evt) {
+  evt.preventDefault();
+  const title = formTitleInput.value;
+  const link = formLinkInput.value;
+  if (title && link) {
+    const NewCard = [
+      {
+        name: title,
+        link: link
+      }
+    ]
+    addElementsCards(NewCard);
   }
-
-  function openPopupAddArticle() {
-    popupAddArticle.classList.add('popup_opened');
-  }
-
-
-  function closePopupEditProfile() {
-    popupEditProfile.classList.remove('popup_opened');
-  }
-
-  function closePopupAddArticle() {
-    popupAddArticle.classList.remove('popup_opened');
-  }
-
-  function closePopupImage() {
-    popupImage.classList.remove('popup_opened');
-  }
-
-
-  function editProfileSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-    profileName.textContent = formNameInput.value;
-    profileJob.textContent = formJobInput.value;
-    closePopupEditProfile();
-  }
-
-  function addArticleSubmitHandler (evt) {
-    evt.preventDefault();
-    const title = formTitleInput.value;
-    const link = formLinkInput.value;
-    if (title && link) {
-      const NewCard = [
-        {
-          name: title,
-          link: link
-        }
-      ]
-      //console.log(NewCards);
-      addElementsCards(NewCard);
-    }
-    closePopupAddArticle();
-  }
+  closePopupAddArticle();
+}
 
 
 
 
 
-  function addElementsCards(userElementsData) {
-    const elementTemplate = document.querySelector('#element').content;
-    const elementsContainer = document.querySelector('.elements');
+function addElementsCards(userElementsData) {
+  const elementTemplate = document.querySelector('#element').content;
+  const elementsContainer = document.querySelector('.elements');
 
-    // создадим из массива дел массив элементов
-    const cardsElements = userElementsData.map(el => {
+  // создадим из массива дел массив элементов
+  const cardsElements = userElementsData.map(el => {
 
-      //клонируем
-      const articleItem = elementTemplate.querySelector('.element').cloneNode(true);
+    //клонируем
+    const articleItem = elementTemplate.querySelector('.element').cloneNode(true);
 
-      // наполняем содержимым
-      articleItem.querySelector('.element__title').textContent = el['name'];
-      articleItem.querySelector('.element__image').src = el['link'];
-      articleItem.querySelector('.element__image').alt = el['name']+ ' фото';
-      // добавим обработчик клика по like
-      articleItem.querySelector('.element__like-icon').addEventListener('click', function(event) {
-        event.target.classList.toggle('element__like-icon_active');
-      });
-      // добавим обработчик клика по delete
-      articleItem.querySelector('.element__delete-icon').addEventListener('click', function(event) {
-        event.target.closest('.element').remove();
-      });
-      // добавим обработчик клика по картинке
-      articleItem.querySelector('.element__image').addEventListener('click', function(event) {
-        //console.log(event.target.src);
-        //console.log(event.target.parentNode.querySelector('.element__title').textContent);
-        showImage(event.target.src, event.target.parentNode.querySelector('.element__title').textContent);
-      });
-
-      return articleItem;
+    // наполняем содержимым
+    articleItem.querySelector('.element__title').textContent = el['name'];
+    articleItem.querySelector('.element__image').src = el['link'];
+    articleItem.querySelector('.element__image').alt = el['name']+ ' фото';
+    // добавим обработчик клика по like
+    articleItem.querySelector('.element__like-icon').addEventListener('click', function(event) {
+      event.target.classList.toggle('element__like-icon_active');
     });
-     // добавим элементы в DOM, «разложив» массив
-     elementsContainer.prepend(...cardsElements);
-  }
-
-  function showImage(url, caption) {
-    const imgUrl = popupImage.querySelector('.popup__image');
-    const imgCaption = popupImage.querySelector('.popup__figcaption');
-    console.log(url);
-    console.log(caption);
-    if (url && caption) {
-      imgUrl.src = url;
-      imgCaption.textContent = caption;
-      popupImage.classList.add('popup_opened');
-    }
-
-  }
-
-
-
-
-
-  formEditProfile.addEventListener('submit', editProfileSubmitHandler);
-  formAddArticle.addEventListener('submit', addArticleSubmitHandler);
-  closePopupLinkProfile.addEventListener('click', closePopupEditProfile);
-  closePopupLinkArticle .addEventListener('click', closePopupAddArticle);
-  closePopupLinkImage.addEventListener('click', closePopupImage);
-  profileEditLink.addEventListener('click', openPopupEditProfile);
-
-  addButton.addEventListener('click', openPopupAddArticle);
-
-  addElementsCards(initialCards);
-
-  /*
-  let likeIcons = document.querySelectorAll('.element__like-icon');
-  console.log('test');
-  Array.from(likeIcons).forEach(icon => {
-    icon.addEventListener('click', function(event) {
-        event.target.classList.toggle('element__like-icon_active');
+    // добавим обработчик клика по delete
+    articleItem.querySelector('.element__delete-icon').addEventListener('click', function(event) {
+      event.target.closest('.element').remove();
     });
+    // добавим обработчик клика по картинке
+    articleItem.querySelector('.element__image').addEventListener('click', function(event) {
+      //console.log(event.target.src);
+      //console.log(event.target.parentNode.querySelector('.element__title').textContent);
+      showImage(event.target.src, event.target.parentNode.querySelector('.element__title').textContent);
+    });
+
+    return articleItem;
   });
-  */
+  // добавим элементы в DOM, «разложив» массив
+  elementsContainer.prepend(...cardsElements);
+}
 
+function showImage(url, caption) {
+  const imgUrl = popupImage.querySelector('.popup__image');
+  const imgCaption = popupImage.querySelector('.popup__figcaption');
+  if (url && caption) {
+    imgUrl.src = url;
+    imgCaption.textContent = caption;
+    popupImage.classList.add('popup_opened');
+  }
+}
 
-  //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal_img
+formEditProfile.addEventListener('submit', editProfileSubmitHandler);
+formAddArticle.addEventListener('submit', addArticleSubmitHandler);
+
+closePopupLinkProfile.addEventListener('click', closePopupEditProfile);
+closePopupLinkArticle .addEventListener('click', closePopupAddArticle);
+closePopupLinkImage.addEventListener('click', closePopupImage);
+
+profileEditLink.addEventListener('click', openPopupEditProfile);
+addButton.addEventListener('click', openPopupAddArticle);
+
+addElementsCards(initialCards);
