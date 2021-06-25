916d5739c0aebf2b09962c8ef1d6398eda6a742f
Ivan Baryutin <vaness97@gmail.com>
2021-06-25 14:26:36 +0300

    add: Добавить класс UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,14 +1,13 @@
 export class UserInfo {
-  constructor(usernameSelector, jobSelector ) {
+  constructor({ usernameSelector, jobSelector }) {
     this._usernameSelector = usernameSelector;
     this._jobSelector = jobSelector;
     this._userInfo = {};
   }
 
-  setUserInfo = () => {
-    this._userInfo.name = document.querySelector(this._usernameSelector).textContent;
-    this._userInfo.job = document.querySelector(this._jobSelector).textContent;
-    return this._userInfo;
+  setUserInfo = (name, job) => {
+    document.querySelector(this._usernameSelector).textContent = name;
+    document.querySelector(this._jobSelector).textContent = job;
   }
 
   getUserInfo = () => {
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,6 +28,7 @@ import { Section } from '../components/Section.js';
 import { Popup } from '../components/Popup.js';
 import { PopupWithImage } from '../components/PopupWithImage.js';
 import { popupWithForm } from '../components/PopupWithForm.js';
+import { UserInfo } from '../components/UserInfo.js';
 
 let openedPopup;
 
@@ -125,15 +126,15 @@ const enableOverlayClose = () => {
 
 enableOverlayClose();
 */
+
+const userInfo = new UserInfo({usernameSelector: '.profile__name', jobSelector: '.profile__subtitle'});
+
 const popupAddArticleNew = new popupWithForm(
   '.popup_content_article',
    (inputData) => {
 
     const card = new Card({name: inputData.title, link: inputData.link}, {
       handleCardClick: (event) => {
-        //console.log(item.name);
-        //console.log(item.link);
-        //console.log(popupWithImageNew);
         popupWithImageNew.open(inputData.link, inputData.title);
       }
     },
@@ -145,13 +146,28 @@ const popupAddArticleNew = new popupWithForm(
 
 });
 
-
 popupAddArticleNew.setEventListeners();
 addButton.addEventListener('click', () => popupAddArticleNew.open());
 
-const popupEditProfileNew = new popupWithForm('.popup_content_profile');
+
+
+
+const popupEditProfileNew = new popupWithForm(
+  '.popup_content_profile',
+    (inputData) => {
+      console.log(inputData);
+      userInfo.setUserInfo(inputData.name, inputData.job);
+});
+
 popupEditProfileNew.setEventListeners();
-profileEditLink.addEventListener('click', () => popupEditProfileNew.open());
+profileEditLink.addEventListener('click', () => {
+  popupEditProfileNew.open();
+  const inputValues = userInfo.getUserInfo();
+  formNameInput.value = inputValues.name;
+  formJobInput.value = inputValues.job;
+});
+
+
 
 const formEditProfileValidator = new FormValidator(settings, formEditProfile);
 formEditProfileValidator.enableValidation();
@@ -164,19 +180,11 @@ const popupWithImageNew = new PopupWithImage('.popup_content_image');
 popupWithImageNew.setEventListeners();
 
 
-//const popupAddArticleNew = new Popup('.popup_content_article');
-//popupAddArticleNew.setEventListeners();
-
-
-
 // Добавляем начальные карточки
 const defaultCardList = new Section({ data: initialCards,
   renderer: (item) => {
   const card = new Card({name: item.name, link: item.link}, {
     handleCardClick: (event) => {
-      //console.log(item.name);
-      //console.log(item.link);
-      //console.log(popupWithImageNew);
       popupWithImageNew.open(item.link, item.name);
     }
   },
